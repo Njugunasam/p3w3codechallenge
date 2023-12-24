f7187bff69e15ff87869289c1b66022fe410dbc4
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-24 12:01:42 +0300

    created restaurant table

--- models.py
+++ models.py
@@ -4,3 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+    reviews = relationship('Review', back_populates='restaurant')
+
